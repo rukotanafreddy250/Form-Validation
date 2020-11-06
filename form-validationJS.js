5b6c90bfc0e61594666ff0f35c327c564dfbe9c7
Rukotana Freddy <rukotanafreddy@gmail.com>
2020-11-07 01:23:59 +0200

    Initial Commit

--- form-validationJS.js
+++ form-validationJS.js
@@ -12,15 +12,69 @@ function showError(input, message){
     const small = formControl.querySelector('small');
     small.innerText = message;
 }
- 
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = ' form-control success';
+    const small = formControl.querySelector('small');
+    // small.innerText = message;
+}
+// function Email validator
+function emailValidator(email){
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+// check all fields
+    function checkRequired(inputArr){
+        inputArr.forEach(function(input){
+            console.log(input);
+            console.log(input.id);
+            if(input.value.trim() === ""){
+                showError(input, ` ${getFieldName(input)} is Required`);
+            }else{
+                showSuccess(input);
+            }
+        });
+    }
+
+// getFieldName
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+
 // add event listeners
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(username.value);
+    checkRequired([username, email, password, password2]);
 
-    if(username.value === ''){
-        showError(username, 'Username is Required');
-    }else{  
-        showError(username);
-    }
+
+    // if(username.value === ''){
+    //     showError(username, 'Username is Required');
+    // }else{  
+    //     showSuccess(username);
+    // }
+
+    // if(email.value === ''){
+    //     showError(email, 'Email is Required');
+    // }else if(!emailValidator(email.value)){
+    //     showError(email, 'Email Is Not Valid');
+    // }else{
+    //     showSuccess(email);
+    // }
+
+    // if(password.value === ''){
+    //     showError(password, 'Password is Required');
+    // }else{  
+    //     showSuccess(password);
+    // }
+    
+    // if(password2.value === ''){
+    //     showError(password2, 'Confirmation Password is Required');
+    // }else{
+    //     showSuccess(password2);
+    // }
 });
